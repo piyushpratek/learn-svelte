21a668fe8040b47bb6f26b462c3022d3a45658dd
Piyush Prateek <piyushprateek231996@gmail.com>
2025-01-25 00:12:29 +0530

    learn svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Page2 from './+page2.svelte';
+	let question = 'What is Svelte?';
+	let src = 'image';
+</script>
+
+<h1>{question}</h1>
+<h1>{question.toUpperCase()}</h1>
+<p>
+	Svelte is a tool for building web applications. Like other user interface frameworks, it allows
+	you to build your app declaratively out of components that combine markup, styles and behaviours.
+	These components are compiled into small, efficient JavaScript modules that eliminate overhead
+	traditionally associated with UI frameworks.
+</p>
+
+<img {src} alt="A {src} dances." />
+
+<p>This is a paragraph.</p>
+
+<Page2 />
+
+<style>
+	p {
+		color: brown;
+		font-family: Arial, Helvetica, sans-serif;
+		font-size: 2em;
+	}
+</style>
